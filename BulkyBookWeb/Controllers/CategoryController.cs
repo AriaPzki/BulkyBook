ce428ff70021984874fc1b40e2f86a1aea5f6edf
AriaPzki <AriaPzki@gmail.com>
2023-10-18 18:46:17 -0400

    retrieve first data

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            // Dbset name = Categories
+            var objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
